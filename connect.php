8b50ffa004603faedc35170aab145658f35702a1
Jaineel Shah <jaineel.ns@somaiya.edu>
2020-10-05 19:17:40 +0530

    config added

---
+++ connect.php
@@ -0,0 +1,11 @@
+<?php
+    $database_name = "ccc_task";
+    $mysql_username = "root";
+    $mysql_password = "";
+    $server_name = "localhost";
+    $conn = mysqli_connect($server_name, $mysql_username, $mysql_password, $database_name);
+
+    $connect = mysqli_connect($server_name, $mysql_username, $mysql_password, $database_name);
+
+    $db = mysqli_connect($server_name, $mysql_username, $mysql_password, $database_name);
+?>
